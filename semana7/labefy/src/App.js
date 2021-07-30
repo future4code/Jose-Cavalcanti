d9903718ab006b116af282708f124616c50d4822
jose henrique holanda cavalcanti <josehenrique.holanda1@gmail.com>
2021-07-30 15:09:30 -0400

    labefy inicado

---
+++ semana7/labefy/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>oiii</h1>
+      </div>
+    );
+  }
+}
