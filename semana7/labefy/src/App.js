ae2e8530c137f3b9c8b67599d19af8a5f2af57c3
jose henrique holanda cavalcanti <josehenrique.holanda1@gmail.com>
2021-08-03 15:43:28 -0400

    quase pronto

--- semana7/labefy/src/App.js
+++ semana7/labefy/src/App.js
@@ -1,12 +1,44 @@
-import React from "react";
-import axios from "axios";
+import React from "react"
+//import styled from "styled-components";
+import TelaCadastroPlaylist from "./components/TelaCadastroPlaylist";
+import TelaDetalhesPlaylist from "./components/TelaDetalhesPlaylist";
+import TelaListaPlaylist from "./components/TelaListaPlaylist";
 
 export default class App extends React.Component {
+  state = {
+    telaAtual: "cadastro"
+  }
+
+  escolheTela = () => {
+    switch (this.state.telaAtual){
+      case "cadastro":
+        return <TelaCadastroPlaylist irParaLista={this.irParaLista}/>
+      case "lista":
+        return <TelaListaPlaylist irParaCadastro={this.irParaCadastro} irParaDetalhes={this.irParaDetalhes}/>
+      case "detalhes":
+        return <TelaDetalhesPlaylist irParaLista={this.irParaLista}/>
+      default:
+        return <div>Erro! Pagina nao encontrada</div>
+    }
+  }
+
+  irParaCadastro = () => {
+    this.setState({telaAtual:"cadastro"})
+  }
+
+  irParaLista = () => {
+    this.setState({telaAtual: "lista"})
+  }
+
+  irParaDetalhes = () => {
+    this.setState({telaAtual: "detalhes"})
+  }
+
   render() {
-    return (
+    return ( 
       <div>
-        <h1>oiii</h1>
+        {this.escolheTela()}
       </div>
-    );
+    )
   }
 }
