edfc9756916ae6f324333e5fb39e92369e506480
jose henrique holanda cavalcanti <josehenrique.holanda1@gmail.com>
2021-09-20 16:05:49 -0400

    exercicios de node

---
+++ semana15/packageJsonNode/src/ex1/index.js
@@ -0,0 +1,5 @@
+const imput = process.argv[2]
+const imput2 = Number(process.argv[3])
+
+console.log("ola", imput, "voce tem", imput2, "anos")
+console.log("ola", imput, "daqui 5 anos voce tera", imput2+7, "anos")
